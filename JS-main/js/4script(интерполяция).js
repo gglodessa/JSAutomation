9ee066e88dcddabfcffe2c047bf1444f50dc75eb
gglodessa <60389816+gglodessa@users.noreply.github.com>
2022-04-28 15:34:28 +0300

    delet

--- JS-main/js/4script(интерполяция).js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-const category = 'toys';
-//console.log('https;//google.ru/' + category + '/' + '4'); //так не красиво
-console.log(`https;//google.ru/${category}/5`); //так красиво))))
-
-const user ='Alex';
-alert(`привет, ${user}`);
-
